2fd13a12b4893c496b7fb69ae9a66a5cdc38d583
Roopali Raizada <roopaliraizada@gmail.com>
2016-10-19 17:51:09 -0700

    Intial Commit

---
+++ src/main/java/hive/HiveJdbcClientExample.java
@@ -0,0 +1,73 @@
+package hive;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class HiveJdbcClientExample {
+	/*
+	 * 
+	 * Before Running this example we should start thrift server. To Start
+	 * Thrift server we should run below command in terminal 
+	 * hive --service hiveserver &
+	 */
+	private static String driverName = "org.apache.hive.jdbc.HiveDriver";
+
+	public static void main(String[] args) throws SQLException {
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		Connection con = DriverManager.getConnection(
+				"jdbc:hive2://localhost:10000/default", "rraizada", "impetus123");
+		Statement stmt = con.createStatement();
+		ResultSet res = null;
+		String tableName = "orders";
+		//stmt.executeQuery("drop table " + tableName);
+		//ResultSet res = stmt.executeQuery("create table " + tableName
+			//	+ " (id int, name string, dept string)");
+
+		// show tables
+		String sql = "show tables"; //'" + tableName + "'";
+		System.out.println("Running: " + sql);
+		res = stmt.executeQuery(sql);
+		if (res.next()) {
+			System.out.println(res.getString(1));
+		}
+
+	    // describe table
+	    sql = "describe " + tableName;
+	    System.out.println("Running: " + sql);
+	    res = stmt.executeQuery(sql);
+	    while (res.next()) {
+	      System.out.println(res.getString(1) + "\t" + res.getString(2) + "\t" + res.getString(2));
+	    }
+	    
+	    // load data into table
+	    // NOTE: filepath has to be local to the hive server
+	    // NOTE: /home/user/input.txt is a ctrl-A separated file with three fields per line
+	   /* String filepath = "/Users/rraizada/Documents/eclipse_workspaces/MRP/MapReduce/src/main/resources/input/hiveinput.txt";
+	    sql = "load data local inpath '" + filepath + "' into table " + tableName;
+	    System.out.println("Running: " + sql);*/
+	    res = stmt.executeQuery(sql);
+	    
+		sql = "select * from " +tableName+" where id='1'";
+		res = stmt.executeQuery(sql);
+		// show tables
+		System.out.println("Running: " + sql);
+		res = stmt.executeQuery(sql);
+		while (res.next()) {
+			System.out.println(res.getString(1));
+			System.out.println(res.getString(2));
+			System.out.println(res.getString(3));
+		}
+		res.close();
+		stmt.close();
+		con.close();
+	}
+}
---
+++ src/main/java/map/WordCountMapper.java
@@ -0,0 +1,24 @@
+package map;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+	private final IntWritable ONE = new IntWritable(1);
+	private Text word = new Text();
+
+	public void map(Object key, Text value, Context context)
+			throws IOException, InterruptedException {
+		String line = value.toString();
+		StringTokenizer tokenizer = new StringTokenizer(line);
+		while (tokenizer.hasMoreTokens()) {
+			word.set(StringUtils.lowerCase(tokenizer.nextToken()));
+			context.write(word, ONE);
+		}
+	}
+}
